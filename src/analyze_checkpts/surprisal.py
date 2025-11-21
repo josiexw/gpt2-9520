deee170ed8cbce8f838901777569b81bf8938cfe
Olivia Tang <oliviat@mit.edu>
2025-11-21 01:05:09 -0500

    include batching into surprisal calculation

--- src/analyze_checkpts/surprisal.py
+++ src/analyze_checkpts/surprisal.py
@@ -12,41 +12,54 @@ from typing import Dict, List
 class Experiment():
     def __init__(self,
                  model_name: str,
-                 output_path: str):
+                 output_path: str,
+                 batch_size: int):
         self.model_name = model_name 
         self.output_path = output_path
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        # TODO: Batching 
+        self.batch_size = batch_size
 
         self.model = HookedTransformer.from_pretrained(self.model_name, device=self.device)
         self.model.eval()
         print(f'loaded model {self.model_name} on {self.device}')
+        self.model.tokenizer.padding_side = 'right'
 
-    def compute_surprisal(self, 
-                          prefix: str,
-                          word: str):
-        full_text = prefix.rstrip() + ' ' + word
+    def compute_batches(self,
+                        prefixes: List[str],
+                        word: str):
+        full_text_l = [prefix.rstrip() + ' ' + word for prefix in prefixes]
+        for i in range(0, len(full_text_l), self.batch_size):
+            yield {
+                'prefixes': prefixes[i:i + self.batch_size],
+                'full_texts': full_text_l[i:i + self.batch_size]
+            }
 
-        prefix_tokens = self.model.to_tokens(prefix)
-        full_tokens = self.model.to_tokens(full_text)
-        prefix_len, full_len = prefix_tokens.shape[1], full_tokens.shape[1]
-        num_word_tokens = full_len - prefix_len
+    def compute_surprisal(self,
+                          batch: Dict[str, List[str]]):
+        prefixes, full_texts = batch['prefixes'], batch['full_texts']
+        batch_size = len(prefixes)
 
-        logits, _ = self.model.run_with_cache(full_tokens) # (1, seq_len, d_vocab)
+        prefix_lens = [len(self.model.to_tokens(prefix, prepend_bos=False)[0]) for prefix in prefixes]
+        full_lens = [len(self.model.to_tokens(full_text, prepend_bos=False)[0]) for full_text in full_texts]
+        num_word_tokens = [full - prefix for prefix, full in zip(prefix_lens, full_lens)]
 
-        word_token_positions = list(range(prefix_len, full_len))
+        full_tokens = self.model.to_tokens(full_texts, prepend_bos=False)
+        with torch.no_grad():
+            logits, _ = self.model.run_with_cache(full_tokens) # [B, seq_len, d_vocab]
 
-        total_logprob_e = 0.0
-        for position in word_token_positions:
-            logits = logits[0, position - 1]
-            log_probs = F.log_softmax(logits, dim=-1)
-            token_id = int(full_tokens[0, position].item())
-            total_logprob_e += float(log_probs[token_id].item())
+        total_logprob_e = torch.zeros(batch_size, device=self.device)
+        for i in range(batch_size):
+            word_token_positions = range(prefix_lens[i], full_lens[i])
+            for position in word_token_positions:
+                logits_pos = logits[i, position - 1]
+                log_probs = F.log_softmax(logits_pos, dim=-1)
+                token_id = int(full_tokens[i, position].item())
+                total_logprob_e[i] += log_probs[token_id]
         
-        surprisal_bits = - total_logprob_e / math.log(2.0)
-        return surprisal_bits, num_word_tokens
-    
-    def compute_surprisal_dict(self,
+        surprisal_bits = (- total_logprob_e / math.log(2.0)).tolist()
+        return surprisal_bits, num_word_tokens 
+
+    def compute_surprisal_dict(self, 
                                contexts: Dict[str, List[str]]):
         surprisal_dict = {}
         words = list(contexts.keys())
@@ -54,22 +67,21 @@ class Experiment():
         for word in tqdm(words):
             prefixes = contexts[word]
             surprisals, token_counts = [], []
-            for prefix in prefixes:
-                surprisal_bits, num_word_tokens = self.compute_surprisal(prefix, word)
-                surprisals.append(surprisal_bits)
-                token_counts.append(num_word_tokens)
+            for batch in self.compute_batches(prefixes, word):
+                surprisal_bits, num_word_tokens = self.compute_surprisal(batch)
+                surprisals.extend(surprisal_bits)
+                token_counts.extend(num_word_tokens)
             
-            avg = sum(surprisals) / len(surprisals)
+            avg = sum(surprisals) / len(surprisals) 
             per_token_vals = [s / t for s, t in zip(surprisals, token_counts)]
-            avg_per_token = sum(per_token_vals) / len(per_token_vals)
+            avg_per_token = sum(per_token_vals) / len(per_token_vals) 
 
             surprisal_dict[word] = {
-                'avg_surprisal': avg,
-                'avg_surprisal_per_token_bits': avg_per_token,
-                'surprisals_list': surprisals
+                'avg_surprisal': avg, 
+                'avg_surprisal_per_token': avg_per_token, 
+                'surprisals_list': surprisals 
             }
-
-        return surprisal_dict
+        return surprisal_dict 
 
 def main():
     parser = argparse.ArgumentParser(description='Compute average surprisal per simple word using GPT-2.')
